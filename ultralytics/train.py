6aa36bf884b22c0b085fe9b74ebade0d42e0ceca
scfeld <2011360994@qq.com>
2026-03-07 10:37:08 +0800

    feat: 添加RT-DETR训练脚本及预训练模型文件
    - 新增训练脚本 `train.py`，用于加载RT-DETR模型并进行训练配置
    - 添加预训练模型文件 `yolo26n.pt`
    - 脚本支持指定模型配置、数据集路径及训练超参数

---
+++ ultralytics/train.py
@@ -0,0 +1,20 @@
+import warnings
+warnings.filterwarnings('ignore')
+from ultralytics import RTDETR
+ 
+# 模型配置文件，以相对路径调用，也可以使用绝对路径
+model_yaml_path = './ultralytics/cfg/models/rt-detr/rtdetr-l.yaml'
+#数据集配置文件，以相对路径调用，也可以使用绝对路径
+data_yaml_path = '../../ultralytics_yolo/dataset.yaml'
+if __name__ == '__main__':
+    model = RTDETR(model_yaml_path)
+    # model.load('rtdetr-l.pt') # 加载预训练权重
+    #训练模型
+    results = model.train(data=data_yaml_path,
+                          imgsz=640,
+                          epochs=100,
+                          batch=8,
+                          workers=0,
+                          project='runs/RT-DETR/train',
+                          name='exp'
+                          )
